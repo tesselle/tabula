7ec5b9751d44ed27172d409e759628825aca9950
nfrerebeau <33983845+nfrerebeau@users.noreply.github.com>
2018-11-05 13:55:24 +0100

    Add package documentation and move imports

---
+++ R/tabula.R
@@ -0,0 +1,13 @@
+#' \code{tabula} package
+#'
+#' Analysis, Seriation and Visualization of Archaeological Count Data
+#'
+#' See the README on
+#' \href{https://github.com/jennybc/googlesheets#readme}{GitHub}
+#'
+#' @docType package
+#' @name tabula
+#' @importFrom magrittr %<>%
+#' @importFrom magrittr %>%
+#' @import ggplot2
+NULL
