0c3e7159a0bf85c7aeeaf1d49a7de0d47fda0362
nfrerebeau <33983845+nfrerebeau@users.noreply.github.com>
2018-11-05 11:19:48 +0100

    Update examples

--- inst/examples/ex-beta.R
+++ inst/examples/ex-beta.R
@@ -1,18 +1,16 @@
 # Degree of turnover
 # Data from Magurran 1988, p. 162
-trees <- matrix(
-  c(TRUE, TRUE, TRUE, FALSE, FALSE, FALSE,
-    TRUE, TRUE, TRUE, TRUE, TRUE, TRUE,
-    FALSE, FALSE, TRUE, FALSE, TRUE, FALSE,
-    FALSE, FALSE, FALSE, TRUE, TRUE, TRUE,
-    FALSE, FALSE, FALSE, FALSE, TRUE, TRUE,
-    FALSE, FALSE, FALSE, TRUE, FALSE, TRUE),
-  nrow = 6, ncol = 6, byrow = FALSE,
+trees <- IncidenceMatrix(
+  data = c(1, 1, 1, 0, 0, 0,
+           1, 1, 1, 1, 1, 1,
+           0, 0, 1, 0, 1, 0,
+           0, 0, 0, 1, 1, 1,
+           0, 0, 0, 0, 1, 1,
+           0, 0, 0, 1, 0, 1),
+  nrow = 6, byrow = FALSE,
   dimnames = list(c("1", "2", "3", "4", "5", "6"),
                   c("Birch", "Oak", "Rowan", "Beech", "Hazel", "Holly"))
 )
-# Coerce to incidence matrix
-trees <- as(trees, "IncidenceMatrix")
 
 ## Whittaker's measure
 turnover(trees, "whittaker") # 1
@@ -30,15 +28,13 @@ turnover(trees, "wilson") # 1
 
 # Similarity measures
 # Data from Magurran 1988, p. 166
-birds <- rbind(
-  `unmanaged` = c(1.4, 4.3, 2.9, 8.6, 4.2, 15.7, 2.0, 50, 1, 11.4, 11.4, 4.3,
-                  13.0, 14.3, 8.6, 7.1, 10.0, 1.4, 2.9, 5.7, 1.4, 11.4, 2.9,
-                  4.3, 1.4, 2.9),
-  `managed` = c(0, 0, 0, 2.9, 0, 0, 0, 10, 0, 0, 5.7, 2.5, 5.7, 8.6, 5.7, 2.9,
-                0, 0, 2.9, 0, 0, 5.7, 0, 2.9, 0, 2.9)
+birds <- CountMatrix(
+  data = c(1.4, 4.3, 2.9, 8.6, 4.2, 15.7, 2.0, 50, 1, 11.4, 11.4, 4.3, 13.0,
+           14.3, 8.6, 7.1, 10.0, 1.4, 2.9, 5.7, 1.4, 11.4, 2.9, 4.3, 1.4, 2.9,
+           0, 0, 0, 2.9, 0, 0, 0, 10, 0, 0, 5.7, 2.5, 5.7, 8.6, 5.7, 2.9, 0, 0,
+           2.9, 0, 0, 5.7, 0, 2.9, 0, 2.9),
+  nrow = 2, byrow = TRUE, dimnames = list(c("unmanaged", "managed"), NULL)
 )
-# Coerce to count matrix
-birds <- as(birds, "CountMatrix")
 
 ## Jaccard measure
 ## (presence/absence data)
