3648d27b94fd839f094d07f0d22bbdb99a3756c6
nfrerebeau <33983845+nfrerebeau@users.noreply.github.com>
2019-02-14 12:48:48 +0100

    Add helpers to check data

--- R/utilities.R
+++ R/utilities.R
@@ -1,17 +1,4 @@
-# HELPERS
-
-# Integer numbers
-#
-# Checks if an object only contains integer numbers.
-# @param x A \code{\link{numeric}} object to be checked.
-# @param tolerance A length-one \link{\code{numeric}} vector giving the
-#  tolerance to check within.
-# @return A \link{\code{logical}} depending on whether \code{x} contains integer
-#  numbers.
-# @seealso \link{\code[base]{is.integer}}
-isWholeNumber <- function(x, tolerance = .Machine$double.eps^0.5) {
-  abs(x - round(x)) < tolerance
-}
+# HELPERS TO CHECK DATA
 
 # Binary numbers
 #
@@ -54,25 +41,59 @@ isPeak <- function(x, na.rm = TRUE) {
     isDecreasing(x[i:n], na.rm = na.rm)
 }
 
-# Matrix constructor
+# Positive numbers
 #
-# @inheritParams base::matrix
-# @param rows A \code{link{logical}} scalar indicating if the number of rows is
-#  unspecified.
-# @param cols A \code{link{logical}} scalar indicating if the number of columns
-#  is unspecified.
-# @return A \link{\code{matrix}}.
-buildMatrix <- function(data, nrow, ncol, byrow, dimnames,
-                        rows = FALSE, cols = FALSE) {
-  k <- length(data)
-  if (rows) nrow <- k / ncol
-  if (cols) ncol <- k / nrow
-  if (is.null(dimnames)) {
-    dimnames <- list(1:nrow, paste("V", 1:ncol, sep = ""))
+# Checks if an object only contains positive values.
+# @param x A \code{\link{numeric}} object to be checked.
+# @param strict A \code{\link{logical}} scalar.
+# @return A \link{\code{logical}}.
+isPositive <- function(x, strict = FALSE) {
+  if (!any(is.nan(x))) {
+    if (strict) {
+      !any(x <= 0, na.rm = TRUE)
+    } else {
+      !any(x < 0, na.rm = TRUE)
+    }
   } else {
-    if (is.null(dimnames[[1]])) dimnames[[1]] <- 1:nrow
-    if (is.null(dimnames[[2]])) dimnames[[2]] <- paste("V", 1:ncol, sep = "")
+    FALSE
   }
-  M <- matrix(data, nrow, ncol, byrow, dimnames)
-  return(M)
+}
+
+# Square matrix
+#
+# Checks if a matrix is square.
+# @param x A \code{\link{matrix}} to be checked.
+# @return A \link{\code{logical}}.
+isSquare <- function(x) {
+  if (is.matrix(x)) {
+    nrow(x) == ncol(x)
+  } else {
+    FALSE
+  }
+}
+
+# Symmetric matrix
+#
+# Checks if a matrix is symmetric.
+# @param x A \code{\link{matrix}} to be checked.
+# @return A \link{\code{logical}}.
+isSymmetric <- function(x) {
+  if (is.matrix(x)) {
+    identical(x, t(x))
+  } else {
+    FALSE
+  }
+}
+
+# Integer numbers
+#
+# Checks if an object only contains integer numbers.
+# @param x A \code{\link{numeric}} object to be checked.
+# @param tolerance A length-one \link{\code{numeric}} vector giving the
+#  tolerance to check within.
+# @return A \link{\code{logical}} depending on whether \code{x} contains integer
+#  numbers.
+# @seealso \link{\code[base]{is.integer}}
+isWholeNumber <- function(x, tolerance = .Machine$double.eps^0.5) {
+  abs(x - round(x)) < tolerance
 }
