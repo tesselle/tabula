b88b0183b13926c54ea5c9c5c71c3a270cea1586
nfrerebeau <33983845+nfrerebeau@users.noreply.github.com>
2019-05-27 15:10:44 +0200

    Add some utilities

--- R/utilities.R
+++ R/utilities.R
@@ -1,12 +1,65 @@
 # HELPERS
 
+#' Helpers
+#'
+#' \code{compact} removes elements from a list or vector.
+#' \code{detect} xxx.
+#'
+#' %o% allows for function composition.
+#'
+#' %||% xxx.
+#' @param x An object.
+#' @param f,f1,f2 A \code{\link{function}}. In \code{compact} and \code{detect}
+#'  \code{f} must be a logical predicate.
+#' @param g A \code{\link{function}}.
+#' @param lhs An object.
+#' @param rhs An object.
+#' @return Returns \code{lhs} if not \code{NULL}, else returns \code{rhs}.
+#' @note
+#'  Adapted from H. Wickham's \emph{Avanced R}.
+#' @references
+#'  Wickham, H. (2014). \emph{Advanced R}. London: Chapman & Hall. The R Series.
+#' @examples
+#'  compact(is.null, list("A", NULL, "B"))
+#'  detect(is.na, c(1, 2, NA, 4, 5, NA))
+#'  count(is.na, c(1, 2, NA, 4, 5, NA))
+#'
+#'  (mean %o% range)(1:5)
+#'
+#'  NULL %||% 1
+#'  0 %||% 1
+#' @name helpers
+#' @keywords internal
+NULL
+
+#' @rdname helpers
+`%||%` <- function(lhs, rhs) {
+  if (!is.null(lhs)) lhs else rhs
+}
+#' @rdname helpers
+`%o%` <- function(f, g) {
+  function(...) f(g(...))
+}
+#' @rdname helpers
+compact <- function(f, x) {
+  Filter(Negate(f), x)
+}
+#' @rdname helpers
+detect <- function(f, x) {
+  vapply(x, f, logical(1))
+}
+#' @rdname helpers
+count <- function(f, x) {
+  sum(detect(f, x))
+}
+
 #' UUID v4
 #'
 #' Generates a universally unique identifier (UUID v4).
 #' @param seed A single \code{\link{integer}} specifying the seeds.
 #'  If \code{NULL} (the default) the seed will be re-initialized.
 #' @details
-#'  As it rely on R's internal random number generators and so will suffer
+#'  As it relies on R's internal random number generators and so will suffer
 #'  from the use of \code{\link{set.seed}} in a session, the seed is
 #'  re-initialized during execution (unless \code{seed} is not \code{NULL}).
 #'  To prevent any side effects, the random number generator (RNG) state is
@@ -14,12 +67,16 @@
 #' @return A 36 characters long \code{\link{character}} string.
 #' @seealso \link{set.seed}
 #' @author N. Frerebeau
+#' @name UUID
 #' @keywords internal
+NULL
+
+#' @rdname UUID
 generateUUID <- function(seed = NULL) {
   # Save and restore the random number generator (RNG) state
   if (!exists(".Random.seed", mode = "numeric")) sample(NA)
   old_seed <- .Random.seed
-  on.exit(assign(".Random.seed", old_seed, envir = globalenv()))
+  on.exit(assign(".Random.seed", old_seed, envir = globalenv()), add = TRUE)
   # Set seed
   seed <- if (is.numeric(seed)) seed else NULL
   set.seed(seed = seed)
@@ -41,177 +98,28 @@ generateUUID <- function(seed = NULL) {
     ),
     collapse = "-"
   )
-  return(uuid)
+  uuid
 }
 
-#' Binary numbers
-#'
-#' Checks if an object only contains 0s and 1s.
-#' @param x A \code{\link{numeric}} object to be checked.
-#' @return A \code{\link{logical}} scalar.
-#' @keywords internal
-#' @noRd
-isBinary <- function(x) {
-  # Validation
-  if (!is.numeric(x))
-    stop("A numeric vector is expected.")
-  sum(x == 1 | x == 0) == length(x)
-}
+#' @rdname UUID
+checkUUID <- function(x) {
+  arg <- deparse(substitute(x))
 
-#' Equality within a vector
-#'
-#' Checks for equality among all elements of a vector.
-#' @param x A \code{\link{numeric}} vector to be checked.
-#' @param tolerance A length-one \link{\code{numeric}} vector giving the
-#'  tolerance to check within.
-#' @param na.rm A \code{\link{logical}} scalar specifying if missing values
-#'  (including NaN) should be omitted.
-#' @return A \code{\link{logical}} scalar.
-#' @keywords internal
-#' @noRd
-isEqual <- function(x, tolerance = .Machine$double.eps^0.5, na.rm = TRUE) {
-  # Validation
-  if (!is.numeric(x))
-    stop("A numeric vector is expected.")
-  abs(max(x, na.rm = na.rm) - min(x, na.rm = na.rm)) <= tolerance
-}
+  if (!isScalarCharacter(x) || is.na(x))
+    throwError(arg, must = "be a character string.")
 
-#' Trends
-#'
-#' Checks if a sequence of numbers is monotonically increasing or decreasing.
-#' @param x A \code{\link{numeric}} vector to be checked.
-#' @param na.rm A \code{\link{logical}} scalar specifying if missing values
-#'  (including NaN) should be omitted.
-#' @return A \code{\link{logical}} scalar.
-#' @keywords internal
-#' @rdname trends
-#' @noRd
-isIncreasing <- function(x, na.rm = TRUE) {
-  # Validation
-  if (!is.numeric(x))
-    stop("A numeric vector is expected.")
-  all(x == cummax(x), na.rm = na.rm) & sum(x, na.rm = na.rm) != 0
-}
-#' @rdname trends
-#' @noRd
-isDecreasing <- function(x, na.rm = TRUE) {
-  # Validation
-  if (!is.numeric(x))
-    stop("A numeric vector is expected.")
-  all(x == cummin(x), na.rm = na.rm) & sum(x, na.rm = na.rm) != 0
-}
-
-#' Overlap
-#'
-#' Checks if two data ranges overlap at all.
-#' @param x A \code{\link{numeric}} vector.
-#' @param y A \code{\link{numeric}} vector.
-#' @return A \code{\link{logical}} scalar.
-#' @keywords internal
-#' @noRd
-isOverlapping <- function(x, y) {
-  # Validation
-  if (!is.numeric(x) | !is.numeric(y))
-    stop("Numeric vectors are expected.")
-
-  !(min(x) > max(y) | max(x) < min(y))
-}
-
-#' Positive numbers
-#'
-#' Checks if an object only contains positive values.
-#' @param x A \code{\link{numeric}} object to be checked.
-#' @param strict A \code{\link{logical}} scalar.
-#' @param na.rm A \code{\link{logical}} scalar specifying if missing values
-#'  (including NaN) should be omitted.
-#' @return A \code{\link{logical}} scalar.
-#' @keywords internal
-#' @noRd
-isPositive <- function(x, strict = FALSE, na.rm = TRUE) {
-  # Validation
-  if (!is.numeric(x))
-    stop("A numeric vector is expected.")
-
-  if (strict) {
-    !any(x <= 0, na.rm = na.rm)
-  } else {
-    !any(x < 0, na.rm = na.rm)
+  n <- nchar(x)
+  if (n != 36) {
+    throwError(arg, must = "be a 36 characters long string", not = n)
   }
 }
 
-#' Square matrix
-#'
-#' Checks if a matrix is square.
-#' @param x A \code{\link{matrix}} to be checked.
-#' @return A \code{\link{logical}} scalar.
-#' @keywords internal
-#' @noRd
-isSquare <- function(x) {
-  if (is.matrix(x)) {
-    nrow(x) == ncol(x)
-  } else {
-    FALSE
+#' @rdname UUID
+compareUUID <- function(x, y) {
+  checkUUID(x)
+  checkUUID(y)
+
+  if (x != y) {
+    stop(sprintf("IDs do not match:\n* %s\n* %s", x, y), call. = FALSE)
   }
-}
-
-#' Contain
-#'
-#' Checks if a vector is a strict subset of another one.
-#' @param subset A vector.
-#' @param set A vector.
-#' @return A \code{\link{logical}} scalar.
-#' @keywords internal
-#' @noRd
-isSubset <- function(subset, set) {
-  # Validation
-  n_sub <- length(subset)
-  n_set <- length(set)
-
-  if (n_sub > n_set) {
-    FALSE
-  } else {
-    if (n_sub == n_set) {
-      identical(subset, set)
-    } else {
-      # Split 'set' in chunks of adjacent elements
-      # Each chunk as the same length as 'subset'
-      set2 <- c(set[-n_set], set[-1])
-      k <- length(set2) %% n_sub
-
-      m <- matrix(c(set2, set2[1:(n_sub - k)]), ncol = n_sub, byrow = TRUE)
-      any(apply(X = m, MARGIN = 1, FUN = identical, y = subset))
-    }
-  }
-}
-
-#' Symmetric matrix
-#'
-#' Checks if a matrix is symmetric.
-#' @param x A \code{\link{matrix}} to be checked.
-#' @return A \code{\link{logical}} scalar.
-#' @keywords internal
-#' @noRd
-isSymmetric <- function(x) {
-  if (is.matrix(x)) {
-    identical(x, t(x))
-  } else {
-    FALSE
-  }
-}
-
-#' Integer numbers
-#'
-#' Checks if an object only contains integer numbers.
-#' @param x A \code{\link{numeric}} object to be checked.
-#' @param tolerance A length-one \link{\code{numeric}} vector giving the
-#'  tolerance to check within.
-#' @return A \code{\link{logical}} scalar.
-#' @seealso \code{\link[base]{is.integer}}
-#' @keywords internal
-#' @noRd
-isWholeNumber <- function(x, tolerance = .Machine$double.eps^0.5) {
-  # Validation
-  if (!is.numeric(x))
-    stop("A numeric vector is expected.")
-  abs(x - round(x)) <= tolerance
 }
