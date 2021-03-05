9ba8eec76bdcb0761fa10dd921efa18426d741ce
nfrerebeau <nicolas.frerebeau@u-bordeaux-montaigne.fr>
2021-03-05 16:36:00 +0100

    Prepare for next release

--- R/tabula-package.R
+++ R/tabula-package.R
@@ -4,8 +4,8 @@
 #'  \strong{Type:} \tab Package \cr
 #'  \strong{Version:} \tab 1.6.0 \cr
 #'  \strong{License:} \tab GPL-3 \cr
-#'  \strong{DOI:} \tab \href{https://doi.org/10.5281/zenodo.1489944}{10.5281/zenodo.1489944} \cr
-#'  \strong{JOSS:} \tab \href{https://doi.org/10.21105/joss.01821}{10.21105/joss.01821} \cr
+#'  \strong{DOI:} \tab \doi{10.5281/zenodo.1489944} \cr
+#'  \strong{JOSS:} \tab \doi{10.21105/joss.01821} \cr
 #' }
 #' @author
 #' \strong{Full list of authors and contributors} (alphabetic order)
