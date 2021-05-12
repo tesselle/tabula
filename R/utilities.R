c96cdce8fd94f58901f6f1fd8a5fc5a97e4c6385
nfrerebeau <nicolas.frerebeau@u-bordeaux-montaigne.fr>
2021-05-12 19:13:18 +0200

    Remove unused code

--- R/utilities.R
+++ R/utilities.R
@@ -1,45 +1,5 @@
 # HELPERS
 
-#' Helpers
-#'
-#' \code{compact} removes elements from a list or vector.
-#' \code{detect} finds values in a list or vector according to a given
-#' predicate.
-#' \code{count} counts values in a list or vector according to a given
-#' predicate.
-#' \code{extract} extracts a string form another string based on a pattern.
-#'
-#' \code{\%o\%} allows for function composition.
-#' \code{\%||\%} allows to define a default value.
-#' @param x,y An object.
-#' @param f,g A \code{\link{function}}. In \code{compact}, \code{detect}
-#'  and \code{count} \code{f} must be a \code{\link{logical}} predicate.
-#' @param pattern A \code{\link{character}} string containing a regular
-#'  expression.
-#' @references
-#'  Wickham, H. (2014). \emph{Advanced R}. London: Chapman & Hall. The R Series.
-#' @family utilities
-#' @keywords internal utilities
-#' @noRd
-`%||%` <- function(x, y) {
-  if (!is.null(x) || length(x) != 0) x else y
-}
-`%o%` <- function(f, g) {
-  function(...) f(g(...))
-}
-compact <- function(f, x) {
-  Filter(Negate(f), x)
-}
-detect <- function(f, x) {
-  vapply(x, f, logical(1))
-}
-count <- function(f, x) {
-  sum(detect(f, x))
-}
-extract <- function(x, pattern) {
-  regmatches(x, regexpr(pattern, x))
-}
-
 #' Scales
 #'
 #' @param x A \code{\link{numeric}} vector.
--- tests/testthat/test-utilities.R
+++ tests/testthat/test-utilities.R
@@ -1,24 +1,4 @@
 # Test helpers
-test_that("Compact", {
-  expect_length(compact(is.null, list("A", NULL, "B")), 2)
-})
-test_that("Detect", {
-  expect_type(detect(is.na, c(1, 2, NA, 4, 5, NA)), "logical")
-  expect_equal(sum(detect(is.na, c(1, 2, NA, 4, 5, NA))), 2)
-})
-test_that("Count", {
-  expect_type(count(is.na, c(1, 2, NA, 4, 5, NA)), "integer")
-  expect_equal(count(is.na, c(1, 2, NA, 4, 5, NA)), 2)
-})
-test_that("Function composition", {
-  expect_type((sum %o% range)(1:5), "integer")
-  expect_equal((sum %o% range)(1:5), 6)
-})
-test_that("NULL OR operator", {
-  expect_type(NULL %||% 1, "double")
-  expect_equal(NULL %||% 1, 1)
-  expect_equal(0 %||% 1, 0)
-})
 # test_that("Rolling window", {
 #   mtx <- matrix(data = seq_len(100), nrow = 20)
 #   roll_index <- roll(mtx, window = 3)
