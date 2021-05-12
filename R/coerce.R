297b9859c4c08287157771db260ccb715f8060ac
nfrerebeau <nicolas.frerebeau@u-bordeaux-montaigne.fr>
2021-05-12 17:55:28 +0200

    Update package description (#24)

--- R/coerce.R
+++ R/coerce.R
@@ -13,25 +13,3 @@ as.data.frame.DiversityIndex <- function(x, ..., stringsAsFactors = default.stri
     stringsAsFactors = FALSE
   )
 }
-
-#' @method as.data.frame IncrementTest
-#' @export
-as.data.frame.IncrementTest <- function(x, ..., stringsAsFactors = default.stringsAsFactors()) {
-  data.frame(
-    t = x@statistic,
-    p.value = x@p_value,
-    row.names = colnames(x@data),
-    stringsAsFactors = FALSE
-  )
-}
-
-#' @method as.data.frame DateMCD
-#' @export
-as.data.frame.DateMCD <- function(x, ..., stringsAsFactors = default.stringsAsFactors()) {
-  data.frame(
-    date = x@mcd_values,
-    error = x@mcd_errors,
-    row.names = rownames(x@data),
-    stringsAsFactors = FALSE
-  )
-}
--- tests/testthat/test-utilities.R
+++ tests/testthat/test-utilities.R
@@ -19,25 +19,6 @@ test_that("NULL OR operator", {
   expect_equal(NULL %||% 1, 1)
   expect_equal(0 %||% 1, 0)
 })
-test_that("Row names to column", {
-  mtx <- matrix(data = seq_len(260), nrow = 26, ncol = 2,
-                dimnames = list(LETTERS, NULL))
-
-  mtx2 <- rownames_to_column(mtx, factor = TRUE, id = "id")
-  expect_identical(dim(mtx2), c(26L, 3L))
-  expect_identical(colnames(mtx2), c("id", "col1", "col2"))
-  expect_identical(unname(mtx2[, 1]), as.factor(LETTERS))
-  expect_s3_class(mtx2, "data.frame")
-
-  df <- as.data.frame(mtx)
-  df2 <- rownames_to_column(df, factor = FALSE)
-  expect_identical(dim(df2), c(26L, 3L))
-  expect_identical(colnames(df2), c("id", "V1", "V2"))
-  expect_identical(unname(df2[, 1]), LETTERS)
-  expect_s3_class(df2, "data.frame")
-
-  expect_error(rownames_to_column(LETTERS, factor = TRUE, id = NULL))
-})
 # test_that("Rolling window", {
 #   mtx <- matrix(data = seq_len(100), nrow = 20)
 #   roll_index <- roll(mtx, window = 3)
