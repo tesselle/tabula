efb4d3d0a68e6edbc67725c741925c10743cae6f
nfrerebeau <nicolas.frerebeau@u-bordeaux-montaigne.fr>
2025-05-12 19:13:14 +0200

    Update documentation

--- R/tabula-package.R
+++ R/tabula-package.R
@@ -1,12 +1,17 @@
 #' @details
 #' \tabular{ll}{
-#'  **Package:** \tab tabula \cr
-#'  **Type:** \tab Package \cr
-#'  **Version:** \tab 3.2.1 \cr
-#'  **License:** \tab GPL-3 \cr
-#'  **Zenodo:** \tab \doi{10.5281/zenodo.1489944} \cr
-#'  **JOSS:** \tab \doi{10.21105/joss.01821} \cr
+#'  **Version** \tab 3.2.1 \cr
+#'  **License** \tab GPL-3 \cr
+#'  **CRAN DOI** \tab \doi{10.32614/CRAN.package.tabula} \cr
+#'  **Zenodo DOI** \tab \doi{10.5281/zenodo.1489944} \cr
+#'  **JOSS DOI** \tab \doi{10.21105/joss.01821} \cr
 #' }
+#'
+#' Archéosciences Bordeaux (UMR 6034)\cr
+#' Maison de l'Archéologie\cr
+#' Université Bordeaux Montaigne\cr
+#' 33607 Pessac cedex\cr
+#' France
 #'
 #' @section Package options:
 #'  \pkg{tabula} uses the following [options()] to configure behavior:
@@ -15,28 +20,6 @@
 #'  * `tabula.verbose`: a [`logical`] scalar. Should \R report extra information
 #'    on progress? Defaults to [interactive()].
 #'
-#' @author
-#' **Full list of authors and contributors** (alphabetic order)
-#'
-#' \tabular{ll}{
-#'  Jean-Baptiste Fourvel \tab *CNRS, France* \cr
-#'  Nicolas Frerebeau \tab *Université Bordeaux Montaigne, France* \cr
-#'  Brice Lebrun \tab *Université Bordeaux Montaigne, France* \cr
-#'  Ben Marwick \tab *University of Washington, USA* \cr
-#'  Matthew Peeples \tab *Arizona State University, USA* \cr
-#'  Anne Philippe \tab *Université de Nantes, France* \cr
-#' }
-#'
-#' **Package maintainer**
-#'
-#' Nicolas Frerebeau\cr
-#' \email{nicolas.frerebeau@@u-bordeaux-montaigne.fr}
-#'
-#' Archéosciences Bordeaux (UMR 6034)\cr
-#' Maison de l'Archéologie\cr
-#' Université Bordeaux Montaigne\cr
-#' 33607 Pessac cedex\cr
-#' France
 #' @name tabula-package
 #' @aliases tabula
 #' @docType package
